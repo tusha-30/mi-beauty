56951103575b5a05fc9c00676bee2142f65c98a9
tusha-30 <developer2@ideausher.com>
2025-04-16 18:01:30 +0530

    setup

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+//home page
+
+export default function Home() {
+  return (
+    <div className="">
+ <div className="text-blue-700 bg-gray-200 font-bold">home</div>
+    </div>
+  );
+}
---
+++ src/app/signup/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div className='bg-gray-200 text-blue'>Signup</div>
+  )
+}
+
+export default Signup
